8361009d9f0c3c20e769759093209ed5c010ff73
Luis Leal <95981661+luislealhdz@users.noreply.github.com>
2024-02-23 16:50:15 -0600

    SA-LPD-10

---
+++ src/SALPD10/Main.java
@@ -0,0 +1,32 @@
+package SALPD10;
+
+import java.util.HashMap;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HashMap<String, String> dictionary = new HashMap<>();
+        dictionary.put("gato", "cat");
+        dictionary.put("perro", "dog");
+        dictionary.put("casa", "house");
+        dictionary.put("libro", "book");
+        dictionary.put("hola", "hello");
+        dictionary.put("amigo", "friend");
+        dictionary.put("sol", "sun");
+        dictionary.put("luna", "moon");
+        dictionary.put("agua", "water");
+        dictionary.put("ciudad", "city");
+        dictionary.put("rojo", "red");
+        dictionary.put("verde", "green");
+        dictionary.put("amarillo", "yellow");
+        dictionary.put("coche", "car");
+        dictionary.put("computadora", "computer");
+        dictionary.put("musica", "music");
+        dictionary.put("juego", "game");
+        dictionary.put("tiempo", "weather");
+        dictionary.put("trabajo", "work");
+        dictionary.put("escuela", "school");
+
+	}
+
+}
