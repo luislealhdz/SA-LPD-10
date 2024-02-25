6cbd0eace73fb5cce5ee688c25bd62eae809d016
Luis Leal <95981661+luislealhdz@users.noreply.github.com>
2024-02-25 17:59:41 -0600

    SA-LPD-10

--- src/SALPD10/Main.java
+++ src/SALPD10/Main.java
@@ -1,8 +1,18 @@
 package SALPD10;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
+	private static String[] getRandomWords(Map<String, String> dictionary, int numberOfWords) {
+		List<String> palabras = new ArrayList<>(dictionary.keySet());
+        Collections.shuffle(palabras);
+        return palabras.subList(0, numberOfWords).toArray(new String[0]);
+	}
 
 	public static void main(String[] args) {
 		HashMap<String, String> dictionary = new HashMap<>();
@@ -26,7 +36,30 @@ public class Main {
         dictionary.put("tiempo", "weather");
         dictionary.put("trabajo", "work");
         dictionary.put("escuela", "school");
-
+        
+        
+        String[] words = getRandomWords(dictionary, 5);
+        
+        int correctAnswers = 0;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        for(String spanishWord: words) {
+            System.out.print("Traduce al inglés " + spanishWord + " : ");
+            
+            String wordToTranslate = scanner.nextLine();
+            
+            String translatedWord = dictionary.get(spanishWord);
+                        
+            boolean isCorrect = translatedWord != null && wordToTranslate.equalsIgnoreCase(translatedWord);
+            
+            correctAnswers += isCorrect ? 1 : 0;
+        }
+        
+        scanner.close();
+        
+        System.out.println("Respuestas correctas: " + correctAnswers);
+        System.out.println("Respuestas incorrectas: " + (5 - correctAnswers));
 	}
-
 }
